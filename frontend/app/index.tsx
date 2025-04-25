223aec1e115263da8fd42380ec12c54798758dcd
Elias <eliasnemr@gmail.com>
2025-04-25 14:27:27 +0300

    Simple login, sign up with supabase

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Redirect } from "expo-router";
+import { supabase } from "../libs/supabase"; 
+import { Session } from "@supabase/supabase-js";
 
 const Dashboard = () => {
-    const user = undefined;
+    const [session, setSession] = useState<Session | null>(null)
 
-    if (!user) {
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          setSession(session)
+        })
+         supabase.auth.onAuthStateChange((_event, session) => {
+          setSession(session)
+        })
+      }, [])
+
+
+    if (!session) {
         return <Redirect href="/login" />
     }
 
